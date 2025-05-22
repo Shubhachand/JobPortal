7b8f4680877ce31f5146956dcbb4329f033eba01
Shubhachand <shubhachandpatel2022@gmail.com>
2025-05-22 22:05:09 +0530

    pre final update

--- backend/index.js
+++ backend/index.js
@@ -2,33 +2,59 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables
+dotenv.config();
+
+// Import custom modules
 import connectDB from "./utils/db.js";
 import userRoutes from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import applicationRoute from "./routes/application.route.js";
-import jobRoute from "./routes/job.route.js";
-// environment variable loaded
-dotenv.config();
+import companyRoutes from "./routes/company.route.js";
+import jobRoutes from "./routes/job.route.js";
+import applicationRoutes from "./routes/application.route.js";
+
+// Initialize app
 const app = express();
-
-//middleware
-app.use(express.json()); // to get in json format
-app.use(express.urlencoded({ extended: true })); // to get in urlencoded format
-app.use(cookieParser()); // to get the cookie from the browser so that we can authenticate the user
-// app.use(cors());  // to connect the frontend and backend
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
+const _dirname = path.resolve();
+
+
+// Middleware
+
+
+// Parse incoming JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Parse cookies from the request headers
+app.use(cookieParser());
+
+// Configure CORS for frontend-backend communication
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+
+// Routes
+
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/company", companyRoute);
-//job route
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
+app.use("/api/v1/company", companyRoutes);
+app.use("/api/v1/job", jobRoutes);
+app.use("/api/v1/application", applicationRoutes);
+
+app.use(express.static(path.join(_dirname, 'frontend', 'dist')));
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(_dirname, 'frontend', 'dist', 'index.html'));
+});
+
+// Server + DB Connection
+connectDB();
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  connectDB();
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  
 });
